7045c9e71edd1004c5debeaebab9807ba8f00ab1
jwanp <joohwan726@naver.com>
2024-02-09 19:11:59 +0900

    fix #24 useOAuth 에서 undefined 리턴시 navigate(/)

--- src/hooks/useOAuth.js
+++ src/hooks/useOAuth.js
@@ -1,10 +1,14 @@
-import { useState, useEffect, useSelector } from 'react';
-import axios from 'axios';
+import { useState, useEffect, useSelector, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function useOAuth() {
   const [jwtToken, setJwtToken] = useState(null);
-  // const called = useRef(false); // prevent rerender caused by StrictMode
+  const called = useRef(false); // prevent rerender caused by StrictMode
+  const navigate = useNavigate();
+
   const exchangeToken = async authorizationCode => {
+    if (called.current) return;
+    called.current = true;
     try {
       const response = await fetch(
         `${process.env.REACT_APP_SERVER_URL}/api/auth/kakao`,
@@ -31,7 +35,7 @@ function useOAuth() {
     if (authorizationCode) {
       exchangeToken(authorizationCode);
     }
-  }, []);
+  });
 
   return jwtToken;
 }
